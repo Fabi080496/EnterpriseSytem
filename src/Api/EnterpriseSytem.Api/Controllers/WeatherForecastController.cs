f5caae941718742ba40dd8f033ec7113eecc5fc0
fabian <astradaf@gmail.com>
2026-01-27 21:24:06 -0500

    implement version

--- src/Api/EnterpriseSytem.Api/Controllers/WeatherForecastController.cs
+++ src/Api/EnterpriseSytem.Api/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EnterpriseSytem.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("v1/WeatherForecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries =
