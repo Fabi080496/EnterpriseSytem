3d81c011a7e48781dba0f4484ef4ba054c7e0d7b
fabian <astradaf@gmail.com>
2026-02-06 08:11:04 -0500

    Implement User

---
+++ src/Api/EnterpriseSytem.Api/Controllers/Modules/Identity/UserController.cs
@@ -0,0 +1,39 @@
+﻿using EnterpriseSystem.Module.Identity.Application.Command;
+using EnterpriseSystem.Module.Identity.Application.Querys.GetByUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseSytem.Api.Controllers.Modules.Identity
+{
+    [ApiController]
+    [Route("User")]
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var user = await _mediator.Send(new GetUserByIdQuery(id));
+
+            if (user is null)
+                return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
+
+
